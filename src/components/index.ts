b93a14931825258a6b6fe97c953261c320a389a4
Danielle DSouza <danielle.dsouza@uwaterloo.ca>
2022-07-27 17:32:13 -0400

    Update packages and remove unneeded files

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export {default as Navbar} from './Navbar/Navbar';
+export {default as Button} from './Button/Button';
+export {default as Subtitle} from './Subtitle/Subtitle';
+export {default as Paragraph} from './Paragraph/Paragraph';
+export {default as Icon} from './Icon/Icon';
+export {default as Image} from './Image/Image';
