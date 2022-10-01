5eb0da744adefb943e78d29495a45459b8b26ade
Danielle DSouza <danielle.dsouza@uwaterloo.ca>
2022-10-01 12:09:49 -0400

    Add links and icons to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import CusecLink from "../CusecLink/CusecLink";
+import brandLogo from "../../assets/logo.png";
 import "./Footer.scss";
 
 const Footer = () => {
@@ -13,12 +15,40 @@ const Footer = () => {
     return <CusecLink key={name} to={link} name={name} />;
   });
 
+  const socialLinks = (
+    <>
+      <a target="_blank" href="https://www.instagram.com/cusecofficial">
+        <i className="fa-brands fa-instagram fa-xl"></i>
+      </a>
+      <a target="_blank" href="https://www.tiktok.com/@cusec_official">
+        <i className="fa-brands fa-tiktok fa-xl"></i>
+      </a>
+      <a target="_blank" href="https://ca.linkedin.com/company/cusec">
+        <i className="fa-brands fa-linkedin fa-xl"></i>
+      </a>
+    </>
+  );
+
   return (
-    <div className="FooterWrapper">
-      <div className="Wrapper">
+    <>
+      <div className="FooterRow">
+        <div className="HideOnMobile">
+          <Link key="icon" to="/">
+            <img
+              src={brandLogo}
+              height={48}
+              alt="CUSEC Logo"
+              className="NavbarLogo"
+            />
+          </Link>
+        </div>
         <ul className="NavItemList">{footerLinks}</ul>
+        <div className="HideOnMobile FooterIcon">{socialLinks}</div>
       </div>
-    </div>
+      <div className="ShowOnMobile FooterRow">
+        <div className="FooterIcon">{socialLinks}</div>
+      </div>
+    </>
   );
 };
 
