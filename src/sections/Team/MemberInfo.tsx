6854f71bf2b5e59809b16fa2a1cdfc58664b72c5
Danielle DSouza <danielle.dsouza@uwaterloo.ca>
2022-10-03 15:18:07 -0400

    Add interactive member cards

---
+++ src/sections/Team/MemberInfo.tsx
@@ -0,0 +1,168 @@
+import { UniYear, CusecExec } from "../../types";
+
+type CusecTeam = {
+  name: string;
+  members: CusecExec[];
+};
+
+export const chairs: CusecTeam = {
+  name: "Chairs",
+  members: [
+    {
+      name: "Charmaine Yung",
+      uni: "University of Toronto",
+      year: UniYear.Fifth,
+      major: "Computer Science",
+      quote:
+        "I have a Google Maps list of food places to try. I have over 200 places but they're mostly on the east coast which is a problem. Please approach me at the conference (or DM me) and hit me with your favourite restaurants from your city!",
+      fact: "I do fencing with lightsabers (it's an officially recognized weapon of the French Fencing Federation!) on Thursdays, which I learned about from a guy I met on the subway who does it",
+    },
+  ],
+};
+
+export const logistics: CusecTeam = {
+  name: "Logistics Team",
+  members: [
+    {
+      name: "Sarah Veloso",
+      uni: "University of Manitoba",
+      year: UniYear.Second,
+      major: "Computer Science and Psychology",
+      quote:
+        "Hello! My name is Sarah. I'm in the second year of my double major in computer science and psychology at the University of Manitoba. I love trying new things and meeting new people! Just this year, I fished for the very first time, I learned to play the trombone, and I met someone who bore a striking resemblance to bbno$. I'm honoured and excited to help bring CUSEC back to an in-person conference!",
+      fact: "I play Call of Duty with bots set to rookie level.",
+    },
+    {
+      name: "Denys Popov",
+      uni: "University of Manitoba",
+      year: UniYear.Forth,
+      major: "Computer Science",
+      quote:
+        "Hello everyone, this is going to be my second CUSEC and the first one in-person. I am a 4th year computer science student at University of Manitoba. I absolutely love basketball, boxing, playing the guitar, biking and, of course, coding.",
+      fact: "Too difficult. No fun. No facts.",
+    },
+  ],
+};
+
+export const sponsorship: CusecTeam = {
+  name: "Sponsorship Team",
+  members: [
+    {
+      name: "",
+      uni: "",
+      year: UniYear.First,
+      major: "",
+      quote: "",
+      fact: "",
+    },
+  ],
+};
+
+export const speakers: CusecTeam = {
+  name: "Speakers Team",
+  members: [
+    {
+      name: "",
+      uni: "",
+      year: UniYear.First,
+      major: "",
+      quote: "",
+      fact: "",
+    },
+  ],
+};
+
+export const technology: CusecTeam = {
+  name: "Technology Team",
+  members: [
+    {
+      name: "Melissa Lin",
+      uni: "Stony Brook University",
+      year: UniYear.Forth,
+      major: "Psychology",
+      quote:
+        "I love sushi, cats, and fashion. Some of my other hobbies is playing Valorant with friends, building keyboards, or working on cool design projects!",
+      fact: "My teacher's chicken jumped and sat on my shoulder once",
+    },
+    {
+      name: "Danielle D'Souza",
+      uni: "University of Waterloo",
+      year: UniYear.Third,
+      major: "Computer Science",
+      quote:
+        "Hey! My name is Danielle, and I'm just a girl in love with dance and code - lots of it! School, work, and dance take up most of my hours, but in my free time you can catch me hiking, watching anime, or pulling irresponsible all-nighters with my friends on campus!",
+      fact: "I'm part of a competitive university hip hop dance team.",
+    },
+  ],
+};
+
+export const events: CusecTeam = {
+  name: "Events Team",
+  members: [
+    {
+      name: "",
+      uni: "",
+      year: UniYear.First,
+      major: "",
+      quote: "",
+      fact: "",
+    },
+  ],
+};
+
+export const promotions: CusecTeam = {
+  name: "Promotions Team",
+  members: [
+    {
+      name: "Jordon Hong",
+      uni: "University of Manitoba",
+      year: UniYear.Second,
+      major: "Computer Science",
+      quote:
+        "Hey everybody! I'm Jordon (sounds like Jordan), your friendly neighbourhood promo director. I'm a second year at the University of Manitoba majoring in computer science and minoring in linguistics. I love D&D, Netflix, and listening to music (especially indie, R&B, and kpop). Second time delegate, first time organizer, so I'm very excited to meet new people. If you see me in at the venue and want to chat about music, code, whatever, don't be shy say hi!",
+      fact: "I have a Duolingo streak of 272 in Mandarin and Korean (as of Sept 29) but can barely speak either lol",
+    },
+  ],
+};
+
+export const headDelegates: CusecTeam = {
+  name: "Head Delegates",
+  members: [
+    {
+      name: "Yasin Elmi",
+      uni: "University of Ottawa",
+      year: UniYear.Third,
+      major: "Computer Science",
+      quote:
+        "Hello my name is Yasin Elmi, I am a third year computer science major and I work part-time for the University of Ottawa as an engineering mentor. Something about myself is that I love to learn and interact with others as well as playing video games, basketball and volleyball. I can't wait to meet and learn from so many people at CUSEC 2023!",
+      fact: "I lived in africa for 5 years of my life",
+    },
+    {
+      name: "Ara Nicole Santos",
+      uni: "University of Manitoba",
+      year: UniYear.Second,
+      major: "Computer Science",
+      quote:
+        "Hi everyone, I’m Ara! I’m the Head Delegate for University of Manitoba. I’m so excited to be a part of CUSEC 2023 as this is my first time joining its team and attending it! I’m currently in my second year of university studying Computer Science and Psychology. My hobbies include reading books and listening to music, especially indie pop. I LOVE to eat chocolate chip cookies and drink bubble tea! I’m so happy to be a part of such a supportive team, and I can’t wait to share with y’all what we have planned for the year !!",
+      fact: "! was born with a tooth",
+    },
+    {
+      name: "Mohammad Zafar",
+      uni: "University of Regina",
+      year: UniYear.Second,
+      major: "Software Engineering",
+      quote:
+        "Hey! My name is Mohammad and I'm in my second year of Software Engineering at the University of Regina. I will also be representing Regina as Head Delegate! I love messing around with carpentry and graphic design. See you at CUSEC!",
+      fact: "I can speak 4 languages",
+    },
+    {
+      name: "Abdul-Hakim Skaik",
+      uni: "Concordia University",
+      year: UniYear.Second,
+      major: "Software Engineering ",
+      quote:
+        "Hello everyone! My name is Hakim and I'm a second year Software Engineer @ Concordia. This is my first year @ CUSEC and I'm excited to be the head delegate of Concordia. I consider myself to be a cool person...or at least I'd like to think so, that means feel free to connect with me and have a coffee chat so we can be cool together. If there's anything I love doing is meeting new people from different backgrounds. I can't wait for CUSEC 2023 to meet more cool people Ü .",
+      fact: "I don't sleep without tiktok",
+    },
+  ],
+};
--- src/sections/Team/Team.tsx
+++ src/sections/Team/Team.tsx
@@ -1,33 +1,44 @@
-import "./Team.scss";
 import Paragraph from "../../components/Paragraph/Paragraph";
 import Subtitle from "../../components/Subtitle/Subtitle";
 import ExpandableDiv from "./components/ExpandableDiv";
-
-const members: string[] = [
-  "Chairs",
-  "Logistics Team",
-  "Sponsorship Team",
-  "Speakers Team",
-  "Technology Team",
-  "Events Team",
-  "Promotions Team",
-];
+import {
+  chairs,
+  logistics,
+  sponsorship,
+  speakers,
+  technology,
+  events,
+  promotions,
+  headDelegates,
+} from "./MemberInfo";
+import "./Team.scss";
 
 function Team() {
   return (
     <div className="Team">
-      <div className="Title">
-        <Subtitle>Meet The Team!</Subtitle>
-        <Paragraph>
-          Click on the drop tabs to learn more about the people who make up the
-          different parts of CUSEC.
-        </Paragraph>
-        <div className="Category">
-          <ul>
-            {members.map((member) => (
-              <ExpandableDiv>{member}</ExpandableDiv>
-            ))}
-          </ul>
+      <div className="Section">
+        <div className="CenterText">
+          <Subtitle>Meet The Team!</Subtitle>
+          <Paragraph>
+            Click on the drop tabs to learn more about the people who make up
+            the different parts of CUSEC.
+          </Paragraph>
+        </div>
+        <div className="TeamList">
+          <ExpandableDiv role={chairs.name} members={chairs.members} />
+          <ExpandableDiv role={logistics.name} members={logistics.members} />
+          <ExpandableDiv
+            role={sponsorship.name}
+            members={sponsorship.members}
+          />
+          <ExpandableDiv role={speakers.name} members={speakers.members} />
+          <ExpandableDiv role={technology.name} members={technology.members} />
+          <ExpandableDiv role={events.name} members={events.members} />
+          <ExpandableDiv role={promotions.name} members={promotions.members} />
+          <ExpandableDiv
+            role={headDelegates.name}
+            members={headDelegates.members}
+          />
         </div>
       </div>
     </div>
